b67c94b6b86036240837e5a4908d6a27a2b7c710
Asus <joyfullkiwi@gmail.com>
2015-07-20 20:07:17 +0300

    maps

--- app/src/main/java/com/courses/android/myproject/MainActivity.java
+++ app/src/main/java/com/courses/android/myproject/MainActivity.java
@@ -1,40 +1,102 @@
 package com.courses.android.myproject;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
 
     //second commit
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-        }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu) {
-            // Inflate the menu; this adds items to the action bar if it is present.
-            getMenuInflater().inflate(R.menu.menu_main, menu);
-            return true;
-        }
+    private GoogleMap mGoogleMap;
+    private TextView mTextInfo;
+    private MyLocationListener mMyLocationListener;
+    private LocationManager mLocationManager;
 
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            // Handle action bar item clicks here. The action bar will
-            // automatically handle clicks on the Home/Up button, so long
-            // as you specify a parent activity in AndroidManifest.xml.
-            int id = item.getItemId();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            //noinspection SimplifiableIfStatement
-            if (id == R.id.action_settings) {
-                return true;
+        mTextInfo = (TextView) findViewById(R.id.tvMyLocationLatLng_AM);
+
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mMyLocationListener = new MyLocationListener(this);
+
+        initMap();
+    }
+
+    private void initMap(){
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.googleMapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+        if (mGoogleMap == null) return;
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        mGoogleMap.setMyLocationEnabled(true);
+        mGoogleMap.setOnMapLongClickListener(this);
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(48.6179388, 22.2985991), 14));
+        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mGoogleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+            @Override
+            public View getInfoWindow(Marker marker) {
+                return null;
             }
 
-        return super.onOptionsItemSelected(item);
+            @Override
+            public View getInfoContents(Marker marker) {
+                return null;
+            }
+        });
+    }
+
+    @Override
+    public void onMapLongClick(LatLng latLng) {
+        mGoogleMap.addMarker(new MarkerOptions()
+                .position(latLng)
+                .title("Marker #" + System.currentTimeMillis() % 10000)
+                .snippet("My first MArker")
+                .icon(BitmapDescriptorFactory.defaultMarker()));
+    }
+
+    public void setLocation(Location location){
+        mTextInfo.setVisibility(View.VISIBLE);
+        mTextInfo.setText(
+                "Loc.: " + location.getLatitude() + " | " + location.getLongitude() + "\n" +
+                        "Time: " + System.currentTimeMillis());
+        onMapLongClick(new LatLng(location.getLatitude(), location.getLongitude()));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mLocationManager.removeUpdates(mMyLocationListener);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mLocationManager.requestLocationUpdates(
+                LocationManager.NETWORK_PROVIDER, 0, 0, mMyLocationListener);
     }
 }
