9a1865cb3c0ac15b811a25267d17a58eed6b8e61
Asus <joyfullkiwi@gmail.com>
2015-07-21 20:18:28 +0300

    maps with marker

--- app/src/main/java/com/courses/android/myproject/MainActivity.java
+++ app/src/main/java/com/courses/android/myproject/MainActivity.java
@@ -9,7 +9,9 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,83 +22,137 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class MainActivity extends ActionBarActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+
+public class MainActivity extends Activity {
 
     //second commit
 
-    private GoogleMap mGoogleMap;
-    private TextView mTextInfo;
-    private MyLocationListener mMyLocationListener;
-    private LocationManager mLocationManager;
+    //private GoogleMap mGoogleMap;
+    private GoogleMap mMap;
+    private ArrayList<MyMarker> mMyMarkersArray = new ArrayList<MyMarker>();
+    private HashMap<Marker, MyMarker> mMarkersHashMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextInfo = (TextView) findViewById(R.id.tvMyLocationLatLng_AM);
+        // Initialize the HashMap for Markers and MyMarker object
+        mMarkersHashMap = new HashMap<Marker, MyMarker>();
 
-        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        mMyLocationListener = new MyLocationListener(this);
+        mMyMarkersArray.add(new MyMarker("Brasil", "icon1", Double.parseDouble("-28.5971788"), Double.parseDouble("-52.7309824")));
+        mMyMarkersArray.add(new MyMarker("United States", "icon2", Double.parseDouble("33.7266622"), Double.parseDouble("-87.1469829")));
+        mMyMarkersArray.add(new MyMarker("Canada", "icon3", Double.parseDouble("51.8917773"), Double.parseDouble("-86.0922954")));
+        mMyMarkersArray.add(new MyMarker("England", "icon4", Double.parseDouble("52.4435047"), Double.parseDouble("-3.4199249")));
+        mMyMarkersArray.add(new MyMarker("Espa?a", "icon5", Double.parseDouble("41.8728262"), Double.parseDouble("-0.2375882")));
+        mMyMarkersArray.add(new MyMarker("Portugal", "icon6", Double.parseDouble("40.8316649"), Double.parseDouble("-4.936009")));
+        mMyMarkersArray.add(new MyMarker("Deutschland", "icon7", Double.parseDouble("51.1642292"), Double.parseDouble("10.4541194")));
+        mMyMarkersArray.add(new MyMarker("Atlantic Ocean", "icondefault", Double.parseDouble("-13.1294607"), Double.parseDouble("-19.9602353")));
 
-        initMap();
+        setUpMap();
+
+        plotMarkers(mMyMarkersArray);
     }
 
-    private void initMap(){
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.googleMapFragment);
-        mapFragment.getMapAsync(this);
-    }
+    private void plotMarkers(ArrayList<MyMarker> markers)
+    {
+        if(markers.size() > 0)
+        {
+            for (MyMarker myMarker : markers)
+            {
 
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mGoogleMap = googleMap;
-        if (mGoogleMap == null) return;
-        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-        mGoogleMap.setMyLocationEnabled(true);
-        mGoogleMap.setOnMapLongClickListener(this);
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(48.6179388, 22.2985991), 14));
-        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        mGoogleMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
-            @Override
-            public View getInfoWindow(Marker marker) {
-                return null;
+                // Create user marker with custom icon and other options
+                MarkerOptions markerOption = new MarkerOptions().position(new LatLng(myMarker.getmLatitude(), myMarker.getmLongitude()));
+                markerOption.icon(BitmapDescriptorFactory.fromResource(R.drawable.currentlocation_icon));
+
+                Marker currentMarker = mMap.addMarker(markerOption);
+                mMarkersHashMap.put(currentMarker, myMarker);
+
+                mMap.setInfoWindowAdapter(new MarkerInfoWindowAdapter());
             }
+        }
+    }
 
-            @Override
-            public View getInfoContents(Marker marker) {
-                return null;
+    private int manageMarkerIcon(String markerIcon)
+    {
+        if (markerIcon.equals("icon1"))
+            return R.drawable.icon1;
+        else if(markerIcon.equals("icon2"))
+            return R.drawable.icon2;
+        else if(markerIcon.equals("icon3"))
+            return R.drawable.icon3;
+        else if(markerIcon.equals("icon4"))
+            return R.drawable.icon4;
+        else if(markerIcon.equals("icon5"))
+            return R.drawable.icon5;
+        else if(markerIcon.equals("icon6"))
+            return R.drawable.icon6;
+        else if(markerIcon.equals("icon7"))
+            return R.drawable.icon7;
+        else
+            return R.drawable.icondefault;
+    }
+
+    private void setUpMap()
+    {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null)
+        {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.googleMapFragment)).getMap();
+
+            // Check if we were successful in obtaining the map.
+
+            if (mMap != null)
+            {
+                mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener()
+                {
+                    @Override
+                    public boolean onMarkerClick(com.google.android.gms.maps.model.Marker marker)
+                    {
+                        marker.showInfoWindow();
+                        return true;
+                    }
+                });
             }
-        });
+            else
+                Toast.makeText(getApplicationContext(), "Unable to create Maps", Toast.LENGTH_SHORT).show();
+        }
     }
 
-    @Override
-    public void onMapLongClick(LatLng latLng) {
-        mGoogleMap.addMarker(new MarkerOptions()
-                .position(latLng)
-                .title("Marker #" + System.currentTimeMillis() % 10000)
-                .snippet("My first MArker")
-                .icon(BitmapDescriptorFactory.defaultMarker()));
+    public class MarkerInfoWindowAdapter implements GoogleMap.InfoWindowAdapter
+    {
+        public MarkerInfoWindowAdapter()
+        {
+        }
+
+        @Override
+        public View getInfoWindow(Marker marker)
+        {
+            return null;
+        }
+
+        @Override
+        public View getInfoContents(Marker marker)
+        {
+            View v  = getLayoutInflater().inflate(R.layout.infowindow_layout, null);
+
+            MyMarker myMarker = mMarkersHashMap.get(marker);
+
+            ImageView markerIcon = (ImageView) v.findViewById(R.id.marker_icon);
+
+            TextView markerLabel = (TextView)v.findViewById(R.id.marker_label);
+
+            markerIcon.setImageResource(manageMarkerIcon(myMarker.getmIcon()));
+
+            markerLabel.setText(myMarker.getmLabel());
+
+            return v;
+        }
     }
 
-    public void setLocation(Location location){
-        mTextInfo.setVisibility(View.VISIBLE);
-        mTextInfo.setText(
-                "Loc.: " + location.getLatitude() + " | " + location.getLongitude() + "\n" +
-                        "Time: " + System.currentTimeMillis());
-        onMapLongClick(new LatLng(location.getLatitude(), location.getLongitude()));
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        mLocationManager.removeUpdates(mMyLocationListener);
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        mLocationManager.requestLocationUpdates(
-                LocationManager.NETWORK_PROVIDER, 0, 0, mMyLocationListener);
-    }
 }
